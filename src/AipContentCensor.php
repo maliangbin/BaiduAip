abc58e1f8fd08f52b528e928955a79fe5dde64da
马良彬 <maliangbin@hoge.cn>
2019-01-05 11:00:15 +0800

    create baidu aip sdk

---
+++ src/AipContentCensor.php
@@ -0,0 +1,11 @@
+<?php
+namespace Baidu\Aip;
+
+use Baidu\Aip\AipImageCensor;
+
+/**
+ * 内容审核
+ */
+class AipContentCensor extends AipImageCensor{
+
+}
---
+++ src/test.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package: aip-php-sdk-2.2.2
+ * @description: test.php
+ * @author: maliangbin Email:maliangbin@hoge.cn
+ * @link: http://www.hoge.cn
+ * @version: 1.0
+ * @date: 2019/1/5
+ */
+
+
+require_once 'AipImageCensor.php';
+
+// 你的 APPID AK SK
+const APP_ID = '15345516';
+const API_KEY = 'tlnZuU8CtEkZzOcjK3wfrNO9';
+const SECRET_KEY = 'feew2Gn3XwpKaCCGcOmC1yRmLdR9dov5';
+
+$client = new AipImageCensor(APP_ID, API_KEY, SECRET_KEY);
+
+$res = $client->antiSpam('卧槽尼玛');
+
+var_dump($res);exit;
